bd1871330792b97965472621b7facf22de5e1ae3
Luna <git@l4.pm>
2020-06-07 14:50:05 -0300

    add basic http parsing

--- src/client.zig
+++ src/client.zig
@@ -10,15 +10,33 @@ pub const Client = struct {
     pub fn deinit(self: *@This()) void {
         std.debug.warn("closing connection with {}\n", .{self.connection.address});
         self.connection.file.close();
-
-        // TODO free itself
         self.allocator.destroy(self);
+    }
+
+    fn constructReply(self: *@This()) []const u8 {
+        return try std.fmt.allocPrint(self.allocator, "HTTP/1.1 {}\r\ncontent-type: text/html\r\ncontent-length: {}\r\n\r\n{}", .{ status_code, message.len, message });
+    }
+
+    fn sendResponse(self: *@This(), status_code: usize, message: []const u8) !void {
+        const response = try self.constructReply(status_code, message);
+        defer self.allocator.free(response);
+        try self.connection.file.write(response);
+    }
+
+    fn responseIgnoreError(self: *@This(), status_code: usize, message: []const u8) void {
+        self.sendResponse(status_code, message) catch |err| {
+            std.debug.warn("Failed to send response to client: {}\n", .{err});
+        };
+    }
+
+    fn invalidHTTP(self: *@This(), message: []const u8) void {
+        return self.responseIgnoreError(400, message);
     }
 
     pub fn handle(self: *@This()) !void {
         std.debug.warn("got client!\n", .{});
         while (true) {
-            var buf: [100]u8 = undefined;
+            var buf: [512]u8 = undefined;
             const read_bytes = try self.connection.file.read(&buf);
             if (read_bytes == 0) {
                 // likely close connection (got it from SIGINT'ing curl)
@@ -27,6 +45,28 @@ pub const Client = struct {
             }
 
             const msg = buf[0..read_bytes];
+            var lines = std.mem.split(msg, "\r\n");
+            const http_header = lines.next().?;
+            var header_it = http_header.split(" ");
+
+            const method = header_it.next() orelse {
+                self.invalidHTTP("Invalid HTTP header");
+                self.deinit();
+                break;
+            };
+
+            const path = header_it.next() orelse {
+                self.invalidHTTP("Invalid HTTP header");
+                self.deinit();
+                break;
+            };
+
+            const http_flag = header_it.next() orelse {
+                self.invalidHTTP("Invalid HTTP header");
+                self.deinit();
+                break;
+            };
+
             std.debug.warn("got msg! '{}'\n", .{msg});
         }
     }
