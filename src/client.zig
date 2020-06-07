4b7414f22e40dab6818ee150b5b186f8eaf2b749
Luna <git@l4.pm>
2020-06-07 00:44:14 -0300

    check when read gave 0 bytes

--- src/client.zig
+++ src/client.zig
@@ -8,6 +8,8 @@ pub const Client = struct {
     handle_frame: @Frame(handle),
 
     pub fn deinit(self: *@This()) void {
+        std.debug.warn("closing connection with {}\n", .{self.connection.address});
+        self.connection.file.close();
         // TODO free itself
     }
 
@@ -16,6 +18,12 @@ pub const Client = struct {
         while (true) {
             var buf: [100]u8 = undefined;
             const read_bytes = try self.connection.file.read(&buf);
+            if (read_bytes == 0) {
+                // likely close connection (got it from SIGINT'ing curl)
+                self.deinit();
+                break;
+            }
+
             const msg = buf[0..read_bytes];
             std.debug.warn("got msg! '{}'\n", .{msg});
         }
